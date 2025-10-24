30851c0cf3287ba43d97c732492b614fa192111f
lazonadeldj <rolandobuenodj@gmail.com>
2025-10-24 18:08:50 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('lazonadeldj-cache').then((cache) => {
+      return cache.addAll(['./index.html', './manifest.webmanifest']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
